86844f003070031ff4b556fb23af7bbd6413569a
Rui Araújo <rui.araujo@ieee.org>
2011-11-27 12:51:03 +0000

    Added buttons and sample compile action.

--- desktop_client/src/com/doublecheck/bstworkbench/ui/Editor.java
+++ desktop_client/src/com/doublecheck/bstworkbench/ui/Editor.java
@@ -9,6 +9,7 @@ import javax.swing.text.*;
 import javax.swing.event.*;
 import javax.swing.undo.*;
 
+
 @SuppressWarnings("serial")
 public class Editor extends JFrame {
 	JTextPane textPane;
@@ -26,9 +27,14 @@ public class Editor extends JFrame {
 	protected RedoAction redoAction;
 	protected UndoManager undo = new UndoManager();
 
+	protected CompileAction compileAction;
+	
+	
+	
+	
 	public Editor() {
-		super("TextComponentDemo");
-
+		super("Microcontroller Workbench");
+		compileAction = new CompileAction();
 		// Create the text pane and configure it.
 		textPane = new JTextPane() // line numbers stay in sync
 		{
@@ -72,14 +78,17 @@ public class Editor extends JFrame {
 		// Add the components.
 		getContentPane().add(splitPane, BorderLayout.CENTER);
 		getContentPane().add(statusPane, BorderLayout.PAGE_END);
+		getContentPane().add(addButtons(), BorderLayout.LINE_END);
 
 		// Set up the menu bar.
 		actions = createActionTable(textPane);
+		JMenu fileMenu = createFileMenu();
 		JMenu editMenu = createEditMenu();
-		// JMenu styleM, BorderLayout.enu = createStyleMenu();
+		JMenu actionMenu = createActionMenu();
 		JMenuBar mb = new JMenuBar();
+		mb.add(fileMenu);
 		mb.add(editMenu);
-		// mb.add(styleMenu);
+		mb.add(actionMenu);
 		setJMenuBar(mb);
 
 		// Add some key bindings.
@@ -92,8 +101,50 @@ public class Editor extends JFrame {
 		textPane.getStyledDocument().addUndoableEditListener(new MyUndoableEditListener());
 		textPane.addCaretListener(caretListenerLabel);
 	}
+	
+	private JPanel getPanelAround(final Component c)
+	{
+		final JPanel panel = new JPanel();
+		panel.add(c);
+		return panel;
+		
+	}
 
+	protected JPanel addButtons(){
+		final JPanel buttonsPanel = new JPanel();
+		final GroupLayout layout = new GroupLayout(buttonsPanel);
+		buttonsPanel.setLayout(layout);
+		layout.setAutoCreateGaps(true);
+		layout.setAutoCreateContainerGaps(true);
+		final JButton compile = new JButton(compileAction);
+		final JButton run = new JButton("Run");
+		final JButton step = new JButton("Step");
+		final JButton stop = new JButton("Stop");
+		
+		compile.setAction(compileAction);
+		layout.setHorizontalGroup(
+			layout.createParallelGroup(GroupLayout.Alignment.LEADING)
+				.addComponent(compile)
+				.addComponent(run)
+				.addComponent(step)
+				.addComponent(stop)
+			);
+		layout.setVerticalGroup(
+			layout.createSequentialGroup()
+				.addComponent(compile)
+				.addGap(20)
+				.addComponent(run)
+				.addComponent(step)
+				.addComponent(stop)
+		);
+		
+		layout.linkSize(SwingConstants.HORIZONTAL,compile , run);
+		layout.linkSize(SwingConstants.HORIZONTAL,compile , step);
+		layout.linkSize(SwingConstants.HORIZONTAL,compile , stop);
 
+		
+		return buttonsPanel;
+	}
 
 	// This one listens for edits that can be undone.
 	protected class MyUndoableEditListener implements UndoableEditListener {
@@ -136,6 +187,47 @@ public class Editor extends JFrame {
 
 	}
 
+	// Create the edit menu.
+	protected JMenu createFileMenu() {
+		JMenu menu = new JMenu("File");
+		menu.setMnemonic( 'F' ); // set mnemonic to F
+		 
+		JMenuItem openFile = new JMenuItem("Open File");
+		menu.add( openFile );
+		JMenuItem saveFile = new JMenuItem("Save File");
+		menu.add( saveFile );
+		JMenuItem saveAsFile = new JMenuItem("Save File As");
+		menu.add( saveAsFile );
+		menu.addSeparator();
+
+		JMenuItem aboutItem = new JMenuItem("About");
+		aboutItem.setMnemonic( 'A' );
+		menu.add( aboutItem );
+		
+		aboutItem.addActionListener( new ActionListener()
+			{
+				// display message dialog when user selects About...
+				public void actionPerformed( ActionEvent event )
+				{
+					JOptionPane.showMessageDialog( Editor.this, "Microcontroller Workbench\n\nYes we can!",
+					"About", JOptionPane.PLAIN_MESSAGE );
+				}
+			}
+		);
+		return menu;
+	}
+	
+	// Create the edit menu.
+	protected JMenu createActionMenu() {
+		JMenu menu = new JMenu("Action");
+		menu.setMnemonic( 'A' ); // set mnemonic to F
+		JMenuItem aboutItem = new JMenuItem(compileAction);
+		menu.add(aboutItem);
+		menu.addSeparator();
+		
+		return menu;
+	}
+	
 	// Create the edit menu.
 	protected JMenu createEditMenu() {
 		JMenu menu = new JMenu("Edit");
@@ -233,5 +325,14 @@ public class Editor extends JFrame {
 			}
 		}
 	}
+	class CompileAction extends AbstractAction {
+		public CompileAction() {
+			super("Compile");
+		}
 
+		public void actionPerformed(ActionEvent e) {
+			changeLog.append("Compiling\n");
+		}
+
+	}
 }
