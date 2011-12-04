57d5bb1229feda5a27f8d51495a5addec41a2b15
Rui Araújo <rui.araujo@ieee.org>
2011-12-04 13:09:40 +0000

    Make SirCommand inherit from SdrCommand to reduce duplication.

--- desktop_client/src/com/doublecheck/bstworkbench/compiler/commands/SdrCommand.java
+++ desktop_client/src/com/doublecheck/bstworkbench/compiler/commands/SdrCommand.java
@@ -2,11 +2,12 @@ package com.doublecheck.bstworkbench.compiler.commands;
 
 
 import org.fife.ui.rsyntaxtextarea.Token;
-
 import com.doublecheck.bstworkbench.compiler.parser.ParserException;
 
 public class SdrCommand extends Command {
         
+    protected final static String KEYWORD = "sdr";
+    
     protected final static String TDI = "tdi";
     protected final static String TDO = "tdo";
     protected final static String MASK = "mask";
@@ -25,7 +26,12 @@ public class SdrCommand extends Command {
     }
 
     public String toString(){
-        final StringBuilder internal = new StringBuilder("SDR ");
+        return toString(KEYWORD, numberBytes, tdi, tdo, mask);
+    }
+    
+    protected static String toString( final String type , final byte numberBytes , final Long tdi, final Long tdo, final Long mask){
+        final StringBuilder internal = new StringBuilder(type);
+        internal.append(' ');
         internal.append(numberBytes);
         internal.append(" TDI(");
         internal.append(tdi);
@@ -48,6 +54,10 @@ public class SdrCommand extends Command {
      * @throws ParserException 
      */
     public static SdrCommand parse(Token tok) throws ParserException{
+       return SdrCommand.parseLine(tok, KEYWORD);        
+    }
+
+    protected static SdrCommand parseLine(Token tok , final String type ) throws ParserException{
         if ( tok == null )
             return null;
         byte numberBytes = 0;
@@ -57,65 +67,58 @@ public class SdrCommand extends Command {
         
         tok = tok.getNextToken(); // first should be a whitespace
         if ( tok == null || tok.type == Token.NULL )
-            throw new ParserException("Expected numeric argument after SDR.");
+            throw new ParserException("Expected numeric argument after " + type + ".");
         
         try{
             tok = tok.getNextToken(); // byte count
             if ( tok == null || tok.type != Token.LITERAL_NUMBER_HEXADECIMAL )
-                throw new ParserException("Expected numeric argument after SDR.");
+                throw new ParserException("Expected numeric argument after " + type + ".");
             numberBytes = Byte.parseByte(tok.getLexeme());
+            mask = getMaximumNumber(numberBytes);
         } 
         catch ( NumberFormatException e ) 
         {
-            throw new ParserException("Error parsing the numeric argument (" + tok.getLexeme() +") after SDR.");
+            throw new ParserException("Error parsing the numeric argument (" + tok.getLexeme() +") after " + type + ".");
         }
- 
-
-        tok = tok.getNextToken();  // should be a whitespace
-        if ( tok == null || tok.type == Token.NULL  )
-            throw new ParserException("Expected TDI after SDR.");
+        
+        tok = tok.getNextToken(); // should be a whitespace
+        if ( tok == null || tok.type == Token.NULL )
+            throw new ParserException("Expected TDI after " + type + ".");
+        
         
         //Checking for TDI
         tok = tok.getNextToken(); // byte count
         if ( !tok.getLexeme().equalsIgnoreCase(TDI) )
-            throw new ParserException("Expected TDI after SDR.");
+            throw new ParserException("Expected TDI after " + type + ".");
         ArgumentParserResult args = getArgument(tok,TDI );
         tdi = args.argument;
         tok = args.token;
-
+        
+        
         tok = tok.getNextToken();  // should be a whitespace
         if ( tok == null || tok.type == Token.NULL  )
-            return new SdrCommand(numberBytes,tdi, tdo, mask);
+            return new SirCommand(numberBytes,tdi, tdo, mask);
         
         
-        //Checking for TDO
+        //Checking for TDO ( optional )
         tok = tok.getNextToken(); // byte count
         if ( tok == null || tok.type == Token.NULL  )
-            return new SdrCommand(numberBytes,tdi, tdo, mask);
+            return new SirCommand(numberBytes,tdi, tdo, mask);
         if ( !tok.getLexeme().equalsIgnoreCase(TDO) )
             throw new ParserException("Unexpected token after TDI.");
         args = getArgument(tok,TDO );
         tdo = args.argument;
         tok = args.token;
         
-
-        tok = tok.getNextToken();  // should be a whitespace
+        tok = tok.getNextToken(); // should be a whitespace
         if ( tok == null || tok.type == Token.NULL  )
-            return new SdrCommand(numberBytes,tdi, tdo, mask);       
+            return new SirCommand(numberBytes,tdi, tdo, mask);
         
-        //Checking for MASK
+        //Checking for MASK( optional )
         tok = tok.getNextToken(); // byte count
         if ( tok == null || tok.type == Token.NULL  )
-        {
-            byte numberFFs = (byte) (numberBytes/4);
-            if ( numberFFs*4 < numberBytes )
-                numberFFs++;
-            StringBuilder maskStr = new StringBuilder();
-            for ( byte i = 0; i <numberFFs; ++i   )
-                maskStr.append('F');
-            mask = Long.parseLong(maskStr.toString(),16);
-            return new SdrCommand(numberBytes,tdi, tdo, mask);
-        }
+            return new SirCommand(numberBytes,tdi, tdo, mask);
+
         if ( !tok.getLexeme().equalsIgnoreCase(MASK) )
             throw new ParserException("Unexpected token after TDO.");
         args = getArgument(tok,MASK );
@@ -126,11 +129,7 @@ public class SdrCommand extends Command {
         String extra = getLineEnd(tok);
         if ( extra.length() > 0 )
             throw new ParserException("Unexpected tokens after parsing MASK:\n"+extra.toString());
-        return new SdrCommand(numberBytes,tdi, tdo, mask);        
-
-        
+        return new SirCommand(numberBytes,tdi, tdo, mask);        
     }
-
-
 
 }
--- desktop_client/src/com/doublecheck/bstworkbench/compiler/commands/SirCommand.java
+++ desktop_client/src/com/doublecheck/bstworkbench/compiler/commands/SirCommand.java
@@ -4,41 +4,15 @@ import org.fife.ui.rsyntaxtextarea.Token;
 
 import com.doublecheck.bstworkbench.compiler.parser.ParserException;
 
-public class SirCommand extends Command {
-    
-    
-    protected final static String TDI = "tdi";
-    protected final static String TDO = "tdo";
-    protected final static String MASK = "mask";
-    
-    protected final byte numberBytes;
-    protected final Long tdi;
-    protected final Long tdo;
-    protected final Long mask;
-    
+public class SirCommand extends SdrCommand {
+    protected final static String KEYWORD = "sir";
     
     public SirCommand(final byte numberBytes , final Long tdi, final Long tdo, final Long mask) {
-        this.numberBytes = numberBytes;
-        this.tdi = tdi;
-        this.tdo = tdo;
-        this.mask = mask;
+        super(numberBytes, tdi, tdo, mask);
     }
 
     public String toString(){
-        final StringBuilder internal = new StringBuilder("SIR ");
-        internal.append(numberBytes);
-        internal.append(" TDI(");
-        internal.append(tdi);
-        internal.append( ") ");
-        if ( tdo != null )
-        {
-            internal.append( "TDO(");
-            internal.append(tdo);
-            internal.append( ") MASK(");
-            internal.append(mask);
-            internal.append( ") ");
-        }
-        return internal.toString();
+        return toString(KEYWORD, numberBytes, tdi, tdo, mask);
     }
     
     /**
@@ -48,83 +22,10 @@ public class SirCommand extends Command {
      * @throws ParserException 
      */
     public static SirCommand parse(Token tok) throws ParserException{
-        if ( tok == null )
+        SdrCommand sdr =  SdrCommand.parseLine(tok, KEYWORD); 
+        if ( sdr == null )
             return null;
-        byte numberBytes = 0;
-        Long tdi = null;
-        Long tdo = null;
-        Long mask = null;
-        
-        tok = tok.getNextToken(); // first should be a whitespace
-        if ( tok == null || tok.type == Token.NULL )
-            throw new ParserException("Expected numeric argument after SIR.");
-        
-        try{
-            tok = tok.getNextToken(); // byte count
-            if ( tok == null || tok.type != Token.LITERAL_NUMBER_HEXADECIMAL )
-                throw new ParserException("Expected numeric argument after SIR.");
-            numberBytes = Byte.parseByte(tok.getLexeme());
-        } 
-        catch ( NumberFormatException e ) 
-        {
-            throw new ParserException("Error parsing the numeric argument (" + tok.getLexeme() +") after SIR.");
-        }
-        
-        tok = tok.getNextToken(); // should be a whitespace
-        if ( tok == null || tok.type == Token.NULL )
-            throw new ParserException("Expected TDI after SIR.");
-        
-        
-        //Checking for TDI
-        tok = tok.getNextToken(); // byte count
-        if ( !tok.getLexeme().equalsIgnoreCase(TDI) )
-            throw new ParserException("Expected TDI after SIR.");
-        ArgumentParserResult args = getArgument(tok,TDI );
-        tdi = args.argument;
-        tok = args.token;
-        
-        
-        tok = tok.getNextToken();  // should be a whitespace
-        if ( tok == null || tok.type == Token.NULL  )
-            return new SirCommand(numberBytes,tdi, tdo, mask);
-        
-        
-        //Checking for TDO
-        tok = tok.getNextToken(); // byte count
-        if ( tok == null || tok.type == Token.NULL  )
-            return new SirCommand(numberBytes,tdi, tdo, mask);
-        if ( !tok.getLexeme().equalsIgnoreCase(TDO) )
-            throw new ParserException("Unexpected token after TDI.");
-        args = getArgument(tok,TDO );
-        tdo = args.argument;
-        tok = args.token;
-        
-        //Checking for MASK ( optional )
-        tok = tok.getNextToken(); // byte count
-        if ( tok == null || tok.type == Token.NULL  )
-        {
-            mask = getMaximumNumber(numberBytes);
-            return new SirCommand(numberBytes,tdi, tdo, mask);
-        }
-        
-        //Checking for MASK
-        tok = tok.getNextToken(); // byte count
-        if ( tok == null || tok.type == Token.NULL  )
-        {
-            mask = getMaximumNumber(numberBytes);
-            return new SirCommand(numberBytes,tdi, tdo, mask);
-        }
-        if ( !tok.getLexeme().equalsIgnoreCase(MASK) )
-            throw new ParserException("Unexpected token after TDO.");
-        args = getArgument(tok,MASK );
-        mask = args.argument;
-        tok = args.token;
- 
-        //Check for extra 
-        String extra = getLineEnd(tok);
-        if ( extra.length() > 0 )
-            throw new ParserException("Unexpected tokens after parsing MASK:\n"+extra.toString());
-        return new SirCommand(numberBytes,tdi, tdo, mask);        
+        return new SirCommand(sdr.numberBytes, sdr.tdi, sdr.tdo, sdr.mask);
     }
 
 
